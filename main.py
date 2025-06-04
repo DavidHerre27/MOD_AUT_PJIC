1082669056d081d9f41ad4986a030fc3e661b5ff
David Herrera <carlos_herrera82181@elpoli.edu.co>
2025-06-04 10:12:03 -0500

    Reestructurado main.py para despliegue en Render

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from endpoints import router as endpoints_router
+from auth.routes import router as auth_router
 
-if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000)
+app = FastAPI()
+
+# 🟢 Permitir acceso desde el frontend React
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://192.168.1.8:5173"],  # Permite solicitudes desde el frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# ✅ Conectar rutas de endpoints y autenticación
+app.include_router(endpoints_router)
+app.include_router(auth_router)
